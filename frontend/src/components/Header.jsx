0ce95d727f3ab03922e53a0879fe1f93d4267814
ocamporom <rom101thegreat@gmail.com>
2024-08-24 13:54:33 +0400

    bit by bit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,23 @@
+import RomImage from '../assets/rom12.png';
+
 const Header = () => {
   return (
     <header
       className="bg-cover bg-center h-screen text-white flex items-center justify-center"
-      style={{ backgroundImage: "url('/assets/rom123.png')" }}
+      style={{ height: '77dvh' }}
+      // style= {{backgroundImage: "url('../assets/rom123.png')" }}
     >
-      <div className="bg-gray-700 bg-opacity-70 py-8 sm:py-12 w-full">
+      <div className="bg-gray-700 bg-opacity-70 py-8 sm:py-10 w-full">
         <div className="container mx-auto text-center px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-2">Rom Joses Ocampo</h1>
-          <p className="text-lg sm:text-xl mb-4">Your Web Developer</p>
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-2">
+            {/* <div>
+            <img src={RomImage} alt="rom" className="w-auto h-[500px]" />
+            </div> */}
+            <div className="flex justify-center items-center">
+              <img src={RomImage} alt="rom" className="w-[500] h-[500px] object-top" />
+            </div>
+          </h1>
+          <p className="text-lg sm:text-xl mb-4">Rom, Your Web Developer</p>
           <a
             href="/assets/Rom Joses Ocampo.pdf"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -22,8 +32,6 @@ const Header = () => {
 };
 
 export default Header;
-
-
 
 // const Header = () => {
 //   return (
